60f5d2975e5ea4d478b53eca4291e4f5c1308d5f
Gerald Bauer <gerald.bauer@gmail.com>
2014-09-07 17:39:34 +0200

    up requires

--- lib/sportdb/version.rb
+++ lib/sportdb/version.rb
@@ -3,7 +3,7 @@ module SportDb
 
   MAJOR = 1    ## todo: namespace inside version or something - why? why not??
   MINOR = 9
-  PATCH = 6
+  PATCH = 7
   VERSION = [MAJOR,MINOR,PATCH].join('.')
 
   def self.version

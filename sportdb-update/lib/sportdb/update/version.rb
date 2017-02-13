afdfaf04d104c85e29301a060e161311482581de
Gerald Bauer <gerald.bauer@gmail.com>
2017-02-13 09:18:49 +0100

    add update

---
+++ sportdb-update/lib/sportdb/update/version.rb
@@ -0,0 +1,24 @@
+
+module SportDb
+module Update
+
+  MAJOR = 0    ## todo: namespace inside version or something - why? why not??
+  MINOR = 1
+  PATCH = 1
+  VERSION = [MAJOR,MINOR,PATCH].join('.')
+
+  def self.version
+    VERSION
+  end
+
+  def self.banner
+    "sportdb-update/#{VERSION} on Ruby #{RUBY_VERSION} (#{RUBY_RELEASE_DATE}) [#{RUBY_PLATFORM}]"
+  end
+
+  def self.root
+    "#{File.expand_path( File.dirname(File.dirname(File.dirname(File.dirname(__FILE__)))) )}"
+  end
+
+end # module Update
+end
+

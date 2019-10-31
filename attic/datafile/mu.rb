53cf4295fa60e6c7331dcefdf64646155f53f84f
Gerald Bauer <gerald.bauer@gmail.com>
2019-10-31 15:43:48 +0100

    Create mu.rb

---
+++ attic/datafile/mu.rb
@@ -0,0 +1,12 @@
+################################################
+# Datafile for Mauritius Premier League
+#
+#  use (inside the mu-mauritius/ folder)
+#    $ sportdb build
+
+
+inline do
+  Country.parse 'mu', 'Mauritius', 'MUS', '2_040 km²', '1_261_200'
+end 
+
+football 'mu-mauritius'

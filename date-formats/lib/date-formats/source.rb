d670e550db4bf54f85c15daf14a13ba97a3381a6
Gerald Bauer <gerald.bauer@gmail.com>
2019-11-07 21:41:25 +0100

    Create source.rb

---
+++ date-formats/lib/date-formats/source.rb
@@ -0,0 +1,111 @@
+module DateFormats
+module Source
+
+  MONTH_EN = <<TXT
+January    Jan
+February   Feb
+March      Mar
+April      Apr
+May
+June       Jun
+July       Jul
+August     Aug
+September  Sept  Sep
+October    Oct
+November   Nov
+December   Dec
+TXT
+
+  MONTH_FR = <<TXT
+Janvier    Janv   Jan     ## check janv in use??
+Février    Févr   Fév     ## check fevr in use???
+Mars              Mar
+Avril      Avri   Avr     ## check avri in use??? if not remove
+Mai
+Juin
+Juillet    Juil
+Août
+Septembre  Sept
+Octobre    Octo   Oct     ## check octo in use??
+Novembre   Nove   Nov     ##  check nove in use??
+Décembre   Déce   Déc     ## check dece in use??
+TXT
+
+  MONTH_ES = <<TXT
+Enero      Ene
+Febrero    Feb
+Marzo      Mar
+Abril      Abr
+Mayo       May
+Junio      Jun
+Julio      Jul
+Agosto     Ago
+Septiembre  Sept  Sep  Set    ## check set in use??
+Octubre    Oct
+Noviembre  Nov
+Diciembre  Dic
+TXT
+
+  MONTH_DE = <<TXT
+Jänner  Januar
+Feber   Februar
+März
+April
+Mai
+Juni
+Juli
+August
+September
+Oktober
+November
+Dezember
+TXT
+
+  MONTH_IT = <<TXT
+Gennaio
+Febbraio
+Marzo
+Aprile
+Maggio
+Giugno
+Luglio
+Agosto
+Settembre
+Ottobre
+Novembre
+Dicembre
+TXT
+
+  MONTH_PT = <<TXT
+Janeiro
+Fevereiro
+Março
+Abril
+Maio
+Junho
+Julho
+Agosto
+Setembro
+Outubro
+Novembro
+Dezembro
+TXT
+
+  MONTH_RO = <<TXT
+Ianuarie
+Februarie
+Martie
+Aprilie
+Mai
+Iunie
+Iulie
+August
+Septembrie
+Octombrie
+Noiembrie
+Decembrie
+TXT
+
+end # module Source
+end # module DateFormats
+

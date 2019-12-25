73c9cc9d8e47e2c829d34a8bc0606f9e6aa25e1a
Gerald Bauer <gerald.bauer@gmail.com>
2019-12-25 13:29:29 +0100

    Create br-inline.rb

---
+++ attic/datafile/br-inline.rb
@@ -0,0 +1,12 @@
+################################################
+# Datafile for Brazil / Brasil
+#
+#  use (inside the br-brazil/ folder)
+#    $ sportdb build
+
+
+inline do
+  Country.parse 'br', 'Brazil', 'BRA', '8_514_215 km²', '192_380_000'
+end 
+
+football 'br-brazil'

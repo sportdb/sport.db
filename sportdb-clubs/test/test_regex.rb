c054ee73272abdeb742055b4b6c78a7e15bfb458
Gerald Bauer <gerald.bauer@gmail.com>
2020-01-02 18:55:12 +0100

    Update test_regex.rb

--- sportdb-clubs/test/test_regex.rb
+++ sportdb-clubs/test/test_regex.rb
@@ -9,21 +9,41 @@ require 'helper'
 
 class TestRegex < MiniTest::Test
 
-  ## note: [ ^] will NOT work as expected; will NOT match beginning of line
-  ADDR_RE = %r{  (^|[ ])                     ## space or beginning of line
-                    (~ | /{2,} | \+{2,})
-                  ([ ]|$)                    ## space or end of line
-              }x
-
+  ADDR_MARKER_RE   = SportDb::Import::ClubReader::ADDR_MARKER_RE
+  B_TEAM_MARKER_RE = SportDb::Import::ClubReader::B_TEAM_MARKER_RE
+  
   def test_addr
-    assert  '~ Wien' =~ ADDR_RE
-    assert  'Wien ~' =~ ADDR_RE
-    assert 'Fischhofgasse 12 ~ 1100 Wien' =~ ADDR_RE
-    assert 'Fischhofgasse 12 ++ 1100 Wien' =~ ADDR_RE
-    assert 'Fischhofgasse 12 ++ 1100 Wien' =~ ADDR_RE
-    assert 'Fischhofgasse 12 // 1100 Wien' =~ ADDR_RE
-    assert 'Fischhofgasse 12 /// 1100 Wien' =~ ADDR_RE
+    assert  '~ Wien' =~ ADDR_MARKER_RE
+    assert  'Wien ~' =~ ADDR_MARKER_RE
+    assert 'Fischhofgasse 12 ~ 1100 Wien'   =~ ADDR_MARKER_RE
+    assert 'Fischhofgasse 12 ++ 1100 Wien'  =~ ADDR_MARKER_RE
+    assert 'Fischhofgasse 12 +++ 1100 Wien' =~ ADDR_MARKER_RE
+    assert 'Fischhofgasse 12 // 1100 Wien'  =~ ADDR_MARKER_RE
+    assert 'Fischhofgasse 12 /// 1100 Wien' =~ ADDR_MARKER_RE
 
-    assert_nil 'Atlanta United FC, 2017,  Atlanta   › Georgia' =~ ADDR_RE
+    assert_nil 'Fischhofgasse 12 + 1100 Wien' =~ ADDR_MARKER_RE
+    assert_nil 'Fischhofgasse 12++1100 Wien' =~ ADDR_MARKER_RE
+    assert_nil 'Fischhofgasse 12 / 1100 Wien' =~ ADDR_MARKER_RE
+    assert_nil 'Fischhofgasse 12//1100 Wien' =~ ADDR_MARKER_RE
+    
+    assert_nil 'Atlanta United FC, 2017,  Atlanta   › Georgia' =~ ADDR_MARKER_RE
   end
+  
+  def test_b_team
+    assert 'b)    Rapid Wien II' =~ B_TEAM_MARKER_RE
+    assert '(b)   Rapid Wien II' =~ B_TEAM_MARKER_RE
+    assert '(b.)  Rapid Wien II' =~ B_TEAM_MARKER_RE
+    assert 'ii)   Rapid Wien II' =~ B_TEAM_MARKER_RE
+    assert 'II)   Rapid Wien II' =~ B_TEAM_MARKER_RE
+    assert '(ii.) Rapid Wien II' =~ B_TEAM_MARKER_RE
+    assert '2)    Rapid Wien II' =~ B_TEAM_MARKER_RE
+    assert '(2)   Rapid Wien II' =~ B_TEAM_MARKER_RE
+ 
+    assert_nil '(3)   Rapid Wien II' =~ B_TEAM_MARKER_RE
+    assert_nil '(iii) Rapid Wien II' =~ B_TEAM_MARKER_RE
+    assert_nil 'iii)  Rapid Wien II' =~ B_TEAM_MARKER_RE
+    assert_nil 'c)    Rapid Wien II' =~ B_TEAM_MARKER_RE
+    assert_nil '(c)   Rapid Wien II' =~ B_TEAM_MARKER_RE
+  end
+  
 end # class TestRegex

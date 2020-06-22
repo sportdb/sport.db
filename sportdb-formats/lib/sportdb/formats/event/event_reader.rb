c7ce19c293dd8162192aecff85cbfb2f478e8e4f
Yo Robot <gerald.bauer+yorobot@gmail.com>
2020-06-22 13:03:42 +0200

    up event

--- sportdb-formats/lib/sportdb/formats/event/event_reader.rb
+++ sportdb-formats/lib/sportdb/formats/event/event_reader.rb
@@ -87,7 +87,7 @@ class EventInfo
 
       parse_csv( @txt ).each do |row|
         league_col = row['League']
-        season_col = row['Season']
+        season_col = row['Season'] || row['Year']
         dates_col  = row['Dates']
 
         season = Import::Season.new( season_col )
@@ -127,22 +127,40 @@ class EventInfo
 
 
         teams_col    = row['Clubs'] || row['Teams']
-        matches_col  = row['Matches']
         goals_col    = row['Goals']
 
         ## note: remove (and allow) all non-digits e.g. 370 goals, 20 clubs, etc.
         teams_col    = teams_col.gsub( /[^0-9]/, '' )    if teams_col
-        matches_col  = matches_col.gsub( /[^0-9]/, '' )  if matches_col
         goals_col    = goals_col.gsub( /[^0-9]/, '' )    if goals_col
 
+        teams  =   (teams_col.nil? || teams_col.empty?) ? nil : teams_col.to_i
+        goals  =   (goals_col.nil? || goals_col.empty?) ? nil : goals_col.to_i
+
+        matches_col  = row['Matches']
+        ## note: support additions in matches (played) e.g.
+        #   132 + 63 Play-off-Spiele
+        matches_col   = matches_col.gsub( /[^0-9+]/, '' )  if matches_col
+
+        matches =  if matches_col.nil? || matches_col.empty?
+                     nil
+                   else
+                     if matches_col.index( '+' )  ### check for calculations
+                        ## note: for now only supports additions
+                        matches_col.split( '+' ).reduce( 0 ) do |sum,str|
+                          sum + str.to_i
+                        end
+                     else            ## assume single (integer) number
+                        matches_col.to_i
+                     end
+                  end
 
         rec = EventInfo.new( league:     league,
                              season:     season,
                              start_date: dates[0],
                              end_date:   dates[1],
-                             teams:      (teams_col.nil?   || teams_col.empty?)   ? nil : teams_col.to_i,
-                             matches:    (matches_col.nil? || matches_col.empty?) ? nil : matches_col.to_i,
-                             goals:      (goals_col.nil?   || goals_col.empty?)   ? nil : goals_col.to_i
+                             teams:      teams,
+                             matches:    matches,
+                             goals:      goals
                            )
         recs << rec
       end  # each row

8b999fc5547c58a19ec510398556e0c04e158be0
Gerald Bauer <gerald.bauer@gmail.com>
2020-09-04 09:23:43 +0200

    up

--- monos/lib/mono/git/base.rb
+++ monos/lib/mono/git/base.rb
@@ -107,14 +107,15 @@ class Git   ## make Git a module - why? why not?
   #   --full-tree makes the command run as if you were in the repo's root directory.
   #   -r recurses into subdirectories. Combined with --full-tree, this gives you all committed, tracked files.
   #   --name-only removes SHA / permission info for when you just want the file paths.
-  #   HEAD specifies which branch you want the list of tracked, committed files for. 
+  #   HEAD specifies which branch you want the list of tracked, committed files for.
   #     You could change this to master or any other branch name, but HEAD is the commit you have checked out right now.
   #
   #   see https://stackoverflow.com/questions/15606955/how-can-i-make-git-show-a-list-of-the-files-that-are-being-tracked
- 
-  def self.files( pathspec=nil )  ## e.g. git ls-files .  or git ls-files *.rb or such
-    cmd = 'git ls-files'
-    cmd << " #{pathspec}"   unless pathspec.nil? || pathspec.empty?
+  #
+  #  was:
+
+  def self.files  ## was: e.g. git ls-files .  or git ls-files *.rb or such
+    cmd =  'git ls-tree --full-tree --name-only -r HEAD'  # was:  'git ls-files'
     Shell.run( cmd )
   end
   ## add list_files or ls_files alias - why? why not?
@@ -199,7 +200,7 @@ class GitProject
   def add_all()                 Git.add_all; end
   def commit( message: )        Git.commit( message: message ); end
 
-  def files( pathspec=nil )     Git.files( pathspec ); end
+  def files()                   Git.files; end
 
 end # class GitProject
 

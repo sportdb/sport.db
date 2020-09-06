a57435a8b9fe5e820fcef9ae670fd0961e0154bd
Gerald Bauer <gerald.bauer@gmail.com>
2020-09-06 22:27:32 +0200

    mv monos

--- monos/CHANGELOG.md
+++
@@ -1,3 +0,0 @@
-### 0.0.1 / 2020-08-30
-
-* Everything is new. First release.
--- monos/lib/monos.rb
+++
@@ -1,5 +0,0 @@
-# note: allow require 'monos' too
-#        (in addition to require 'mono')
-
-require_relative './mono'
-

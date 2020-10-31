dd79c255010719efba1b50aad76432e23a9e045e
Gerald Bauer <gerald.bauer@gmail.com>
2020-10-31 17:33:19 +0100

    mv alphabets, date-formats

--- alphabets/CHANGELOG.md
+++
@@ -1,3 +0,0 @@
-### 0.0.1 / 2019-08-13
-
-* Everything is new. First release.
--- date-formats/lib/date/formats.rb
+++
@@ -1,4 +0,0 @@
-# note: allow require 'date/formats' too
-#        (in addition to require 'date-formats')
-
-require_relative '../date-formats'

17dc21ecf3cfd836bd0e2a24611111b3f52bf8a6
Gerald Bauer <gerald.bauer@gmail.com>
2024-08-12 17:26:18 +0200

    up

--- sportdb-formats/lib/sportdb/formats/search/structs.rb
+++ sportdb-formats/lib/sportdb/formats/search/structs.rb
@@ -17,9 +17,9 @@ class Country
     end
    def self.find_by( code: nil, name: nil )
         _search.find_by( code: code, name: name )
-   end 
+   end
 
-   def self.find( q )   ## find by code (first) or name (second) 
+   def self.find( q )   ## find by code (first) or name (second)
        _search.find( q )
    end
 
@@ -28,7 +28,7 @@ class Country
       _search.parse( line )
    end
 
-   ## add alternate names/aliases 
+   ## add alternate names/aliases
    class << self
     alias_method :[],      :find    ### keep shortcut - why? why not?
     alias_method :heading, :parse_heading
@@ -37,14 +37,14 @@ class Country
 
 # open question - what name to use build or  parse_line or ?
 #                              or   parse_recs for CountryReader?
-#          remove CountryReader helper methods - why? why not?   
+#          remove CountryReader helper methods - why? why not?
 #   use parse_heading/heading for now !!!
 #
 #   def self.parse( line )  or build( line ) ??
-#      SportDb::Import.world.countries.parse( line )    
+#      SportDb::Import.world.countries.parse( line )
 #   end
 #
-# !!!! note - conflict with 
+# !!!! note - conflict with
 #     def self.read( path )  CountryReader.read( path ); end
 #     def self.parse( txt )  CountryReader.parse( txt ); end
 #
@@ -52,7 +52,7 @@ end # class Country
 
 
 ###
-## todo/fix - add find_by( code: ), find_by( name: ) 
+## todo/fix - add find_by( code: ), find_by( name: )
 ##                   split - why? why not?
 
 
@@ -60,16 +60,17 @@ class League
     def self._search #### use service/api or such - why? why not?
         SportDb::Import.catalog.leagues
     end
-    def self.match_by( name:, country: nil )
-       _search.match_by( name: name,
-                         country: country ) 
+    def self.match_by( name: nil, code: nil,
+                        country: nil )
+       _search.match_by( name: name, code: code,
+                         country: country )
     end
-    def self.match( name ) match_by( name: name ); end 
-  
-    def self.find!( name ) _search.find!( name ); end  
-    def self.find( name )  _search_find( name ); end
+    def self.match( q ) _search.match( q ); end
+
+    def self.find!( q ) _search.find!( q ); end
+    def self.find( q )  _search_find( q ); end
 end # class League
-  
+
 
 class NationalTeam
     def self._search #### use service/api or such - why? why not?
@@ -79,8 +80,8 @@ class NationalTeam
     def self.find( q )   _search.find( q ); end
     def self.find!( q )  _search_find!( q ); end
 end # class NationalTeam
-  
-  
+
+
 class Club
     def self._search #### use service/api or such - why? why not?
         SportDb::Import.catalog.clubs
@@ -93,21 +94,21 @@ class Club
                           league: league, mods: mods )
     end
     def self.match( name ) match_by( name: name ); end
-   
+
     def self.find( name )   _search.find_by( name: name ); end
     def self.find!( name )  _search.find_by!( name: name ); end
-  
+
     def self.find_by!( name:, country: nil,
-                              league:  nil )  
+                              league:  nil )
        _search.find_by!( name: name, country: country,
                                      league: league )
     end
     def self.find_by( name:, country: nil,
-                             league:  nil )  
+                             league:  nil )
        _search.find_by( name: name, country: country,
-                                    league: league )                      
+                                    league: league )
     end
-    
+
    def self.build_mods( mods )
        _search_build_mods( mods )
     end

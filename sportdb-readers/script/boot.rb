bc255bca7113ead75e458e905212da2480336488
Gerald Bauer <gerald.bauer@gmail.com>
2024-08-21 12:44:25 +0200

    up

--- sportdb-readers/script/boot.rb
+++ sportdb-readers/script/boot.rb
@@ -1,16 +1,24 @@
 ## note: use the local version of sportdb gems
+$LOAD_PATH.unshift( File.expand_path( '../sportdb-structs/lib' ))
 $LOAD_PATH.unshift( File.expand_path( '../sportdb-formats/lib' ))
 $LOAD_PATH.unshift( File.expand_path( '../sportdb-catalogs/lib' ))
 $LOAD_PATH.unshift( File.expand_path( '../sportdb-models/lib' ))
 $LOAD_PATH.unshift( File.expand_path( '../sportdb-sync/lib' ))
+$LOAD_PATH.unshift( './lib' )
+
+$LOAD_PATH.unshift( File.expand_path( '../parser/lib' ))
 
 
 ## our own code
 require 'sportdb/readers'
 
 
+## use (switch to) "external" datasets
+SportDb::Import.config.catalog_path = "../catalog/catalog.db"
+
+## print/dump stats
+CatalogDb::Metal.tables
+
+
 OPENFOOTBALL_PATH = '../../../openfootball'
 
-## use (switch to) "external" datasets
-SportDb::Import.config.leagues_dir = "#{OPENFOOTBALL_PATH}/leagues"
-SportDb::Import.config.clubs_dir   = "#{OPENFOOTBALL_PATH}/clubs"
--- sportdb-readers/script/read_champs.rb
+++ sportdb-readers/script/read_champs.rb
@@ -1,6 +1,6 @@
 ##
 #  use
-#    ruby -I ./lib script/read_champs.rb
+#    ruby script/read_champs.rb
 
 
 require_relative 'boot'
@@ -9,9 +9,11 @@ require_relative 'boot'
 File.delete( './champs.db' )   if File.exist?( './champs.db' )
 
 
-SportDb.connect( adapter:  'sqlite3',
-                 database: './champs.db' )
-SportDb.create_all   ## build schema
+SportDb.open( './champs.db' )
+
+# SportDb.connect( adapter:  'sqlite3',
+#                  database:  )
+# SportDb.create_all   ## build schema
 
 ## turn on logging to console
 ## ActiveRecord::Base.logger = Logger.new(STDOUT)
@@ -25,5 +27,10 @@ pack.read_match
 
 ## pack.read
 ## pack.read( season: '2012/13' )
+
+
+puts "table stats:"
+SportDb.tables
+
 
 puts 'bye'

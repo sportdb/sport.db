d912801375ec6f73b4212b39ea9392f889971f81
Gerald Bauer <gerald.bauer@gmailcom>
2024-12-30 15:56:16 +0100

    up

--- quick/lib/sportdb/quick/version.rb
+++ quick/lib/sportdb/quick/version.rb
@@ -3,8 +3,8 @@ module SportDb
   module Module
     module Quick
   MAJOR = 0    ## todo: namespace inside version or something - why? why not??
-  MINOR = 2
-  PATCH = 1
+  MINOR = 3
+  PATCH = 0
   VERSION = [MAJOR,MINOR,PATCH].join('.')
 
   def self.version
